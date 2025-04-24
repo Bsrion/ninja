ccdd61a1b0920f8f0d7fff8204387cb47388d5c1
mac_pro_2013 <mac_pro_2013@mac-pro-2013s-Mac-Pro.local>
2025-04-24 20:12:37 +0300

    24/04/25 update

--- src/component/pollDetails.svelte
+++ src/component/pollDetails.svelte
@@ -2,19 +2,23 @@
 // @ts-nocheck
     import Card from '../shared/card.svelte'; 
 let {poll = {}} = $props();
-let totalVotes = $derived(poll.voteA + poll.voteB)
+let totalVotes = $derived(poll.voteA + poll.voteB);
+let firstanswer = $derived(Math.floor(poll.voteA / totalVotes * 100));
+let secondanswer = $derived(Math.floor(poll.voteB / totalVotes * 100));
 
 </script>
 <Card>
     <div class="poll">
         <h3>{poll.question}</h3>
         <p>Total votes: {totalVotes}</p>
-        <div class="answer" onclick={()=>{poll.voteA++}}>
-            <div class="percent percent-a"></div>
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div class="answer" onclick={()=>{poll.voteA++}} onkeydown={(e)=>{if(e.key === 'Enter'){poll.voteA++}}}>
+            <div class="percent percent-a" style="width:{firstanswer}%"></div>
             <span>{poll.answerA} ({poll.voteA})</span>
     </div>
-        <div class="answer" onclick={()=>{poll.voteB++}}>
-            <div class="percent percent-b"></div>
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div class="answer" onclick={()=>{poll.voteB++}} onkeydown={(e)=>{if(e.key === 'Enter'){poll.voteB++}}}>
+            <div class="percent percent-b" style="width:{secondanswer}%"></div>
             <span>{poll.answerB} ({poll.voteB})</span>
         </div>
     </div>
@@ -43,5 +47,18 @@ p{
 span{
     display: inline-block;
     padding: 10px;
+}
+.percent{
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+}
+.percent-a{
+    background-color: rgba(217, 27 , 66, 0.2);
+    border-left: solid 4px rgba(217, 27 , 66, 1);
+}
+.percent-b{
+    background-color: rgba(69, 196 , 50, 0.2);
+    border-left: solid 4px rgba(69, 196 , 50, 1);
 }
 </style>
